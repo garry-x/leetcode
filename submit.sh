3740f4d350ae30e178f51f2480934ba90570cd28
Garry Xu <garry.x@outlook.com>
2020-12-11 18:34:34 +0800

    leetcode: optimize submit script
    Add logic to automatically generate commit message
    
    Signed-off-by: Garry Xu <garry.x@outlook.com>

--- submit.sh
+++ submit.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-    echo "$0 \"solution_name\""
+if [ $# -lt 2 ]; then
+    echo "$0 \"solution_name\" \"id\"" 
     exit 1
 fi
 
 prob_name=$1
+prob_id=$2
 
-# clean project
+dir_name=${prob_id}-${prob_name}
 
-cd $prob_name
+if [ ! -d $dir_name ]; then
+   exit 1 
+fi
+
+cd $dir_name
 
 cargo clean
 
@@ -17,6 +22,13 @@ cd ..
 
 git add *
 
-git commit -s
+git commit --author="Garry Xu <garry.x@outlook.com>" \
+        -m "
+leetcode[$prob_id]: add solution for ${prob_name}
+
+REF: https://leetcode.com/problems/${prob_name}
+
+Signed-off-by: Garry Xu <garry.x@outlook.com>
+"
 
 git push
