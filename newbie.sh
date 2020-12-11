ffa22c8b3510a18fa623184a7300545cb5e8840b
Garry Xu <garry.x@outlook.com>
2020-12-11 14:12:49 +0800

    leetcode: refactor author and the helper script
    fix "author" to corret names and change script to support
    problem id.
    
    Signed-off-by: Garry Xu <garry.x@outlook.com>

--- newbie.sh
+++ newbie.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-    echo "$0 \"solution_name\""
+if [ $# -lt 2 ]; then
+    echo "$0 \"solution_name\" \"id\""
     exit 1
 fi
 
 prob_name=$1
+prob_id=$2
 
 # create project
 cargo new --bin --vcs none $prob_name
 
+mv $prob_name "${prob_id}-${prob_name}"
+
 # copy template
-cp template/src/main.rs ${prob_name}/src/ 
+cp template/src/main.rs ${prob_id}-${prob_name}/src/ 
