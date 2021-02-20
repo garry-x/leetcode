46a1bad8a8602ae7018e398a0aee0f34e237bddc
Garry Xu <garry.x@outlook.com>
2021-02-20 16:27:57 +0800

    template: add time measurement in template
    Signed-off-by: Garry Xu <garry.x@outlook.com>

--- template/src/main.rs
+++ template/src/main.rs
@@ -3,5 +3,10 @@ pub struct Solution {}
 impl Solution {
 }
 
+use std::time:: SystemTime;
+
 fn main() {
+    let start = SystemTime::now();
+    
+    println!("Time elapsed:{} us", SystemTime::now().duration_since(start).unwrap().as_micros());
 }
