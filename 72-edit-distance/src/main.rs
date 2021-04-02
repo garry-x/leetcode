4dbbdb65d0746f4df08d82a61e03c55af976e0b4
Garry Xu <garry.x@outlook.com>
2021-04-02 19:02:20 +0800

    leetcode[72]: add solution for edit-distance
    REF: https://leetcode.com/problems/edit-distance
    
    Signed-off-by: Garry Xu <garry.x@outlook.com>

--- 72-edit-distance/src/main.rs
+++ 72-edit-distance/src/main.rs
@@ -1,42 +1,23 @@
 pub struct Solution {}
 
-use std::collections::HashMap;
-
 impl Solution {
     pub fn min_distance(word1: String, word2: String) -> i32 {
-        min_rec(&word1.chars().collect(), &word2.chars().collect(),
-                0, 0, 0, &mut HashMap::new()) as i32
-    }
-}
+        let v1: Vec<char> = word1.chars().collect();
+        let v2: Vec<char> = word2.chars().collect();
 
-pub fn min_rec(v1: &Vec<char>, v2: &Vec<char>, i: usize, j: usize,
-               count: usize, map: &mut HashMap<(usize, usize),
-               usize>) -> usize {
-    // insert
-    if i == v1.len() { return count + v2.len() - j; }
-    // delete
-    if j == v2.len() { return count + v1.len() - i; } 
+        let mut dp: Vec<Vec<usize>> = vec![vec![0; v2.len() + 1]; v1.len() + 1];
+        for i in (0..v1.len() + 1).rev() {
+            for j in (0..v2.len() + 1).rev() {
+                if j >= v2.len() { dp[i][j] = v1.len() - i; continue; }
+                if i >= v1.len() { dp[i][j] = v2.len() - j; continue; }
+                if v1[i] == v2[j] { dp[i][j] = dp[i + 1][j + 1]; continue; }
+                dp[i][j] = std::cmp::min(std::cmp::min(dp[i + 1][j], dp[i + 1][j + 1]),
+                                         dp[i][j + 1]) + 1;
+            }
+        }
 
-    if let Some(&v) = map.get(&(i, j)) {
-        return v + count;
+        dp[0][0] as i32
     }
-
-    let min_count;
-    if v1[i] == v2[j] {
-        // go forward
-        min_count = min_rec(v1, v2, i + 1, j + 1, count, map);
-    } else {
-        // replace
-        let c1 = min_rec(v1, v2, i + 1, j + 1, count + 1, map);
-        // delete
-        let c2 = min_rec(v1, v2, i + 1, j, count + 1, map);
-        // insert
-        let c3 = min_rec(v1, v2, i, j + 1, count + 1, map);
-        min_count = std::cmp::min(std::cmp::min(c1, c2), c3);
-    }
-    
-    map.insert((i, j), min_count - count);
-    min_count
 }
 
 use std::time:: SystemTime;
