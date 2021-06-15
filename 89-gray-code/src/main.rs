b79e050e0c8610481dd337fe8861f591730a7c65
Garry Xu <garry.x@outlook.com>
2021-06-15 11:04:13 +0800

    leetcode[89]: add solution for gray-code
    REF: https://leetcode.com/problems/gray-code
    
    Signed-off-by: Garry Xu <garry.x@outlook.com>

--- 89-gray-code/src/main.rs
+++ 89-gray-code/src/main.rs
@@ -2,25 +2,10 @@ pub struct Solution {}
 
 impl Solution {
     pub fn gray_code(n: i32) -> Vec<i32> {
-        // Bn-1Bn-2..B0
-        // Gn-1Gn-2..G0
-        // Gn-1 = Bn-1
-        // Gi = Bi ^ Bi+1 (0 <= i <= n -2)
-        fn convert_to_gray(v: i32, n: u32) -> i32 {
-            if v == 0 || n == 1 {
-                return v;
-            }
-           
-            let mut out = v & 1 << (n - 1);
-            for i in (0..n-1).rev() {
-                out = out | (1 << i & v) ^ ((1 << (i + 1) & v) >> 1);
-            }
-            out
-        }
-
         let mut out: Vec<i32> = Vec::new();
         for i in 0..2i32.pow(n as u32) {
-            out.push(convert_to_gray(i, n as u32));
+            // https://en.wikipedia.org/wiki/Gray_code
+            out.push((i >> 1) ^ i);
         }
         out
     }
