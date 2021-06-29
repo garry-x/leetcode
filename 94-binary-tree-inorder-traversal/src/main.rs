8cfe35d1f53b794600a9339f135527681e84a0bf
Garry Xu <garry.x@outlook.com>
2021-06-29 08:58:37 -0400

    leetcode[94]: add solution for binary-tree-inorder-traversal
    REF: https://leetcode.com/problems/binary-tree-inorder-traversal
    
    Signed-off-by: Garry Xu <garry.x@outlook.com>

--- 94-binary-tree-inorder-traversal/src/main.rs
+++ 94-binary-tree-inorder-traversal/src/main.rs
@@ -18,31 +18,53 @@ impl TreeNode {
     }
   }
 }
+
 use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
         let mut res: Vec<i32> = Vec::new();
-        travel_rec(&root, &mut res); 
+        
+        let mut stack = Vec::new();
+        let mut cur = root.clone();
+        
+        while cur.is_some() || !stack.is_empty() {
+            while let Some(node) = cur {
+                cur = node.borrow().left.clone();
+                // Rc<RefCell<TreeNode>>
+                stack.push(node);
+            }
+            
+            // RefCell<TreeNode>
+            let node = stack.pop().unwrap();
+            res.push(node.borrow().val);
+            cur = node.borrow().right.clone(); 
+        }
+
         res        
     }
 }
 
-pub fn travel_rec(node: &Option<Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
-    if node.is_none() {
-        return;
+pub fn build_btree(nums: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+    if nums.len().trailing_zeros() != 0 {
+        return None;
     }
 
-    let n = node.as_ref().unwrap().borrow();
-    travel_rec(&n.left, res);
-    res.push(n.val);
-    travel_rec(&n.right, res);
+    Some(Rc::new(RefCell::new(TreeNode {
+        val: nums[0],
+        left: build_btree(&nums[1..nums.len() / 2 + 1]),
+        right: build_btree(&nums[nums.len() / 2 + 1..])}))) 
 }
 
 use std::time::SystemTime;
 
 fn main() {
     let start = SystemTime::now();
+    
+    println!("{:?}", Solution::inorder_traversal(build_btree(&(1..2i32.pow(3)).collect::<Vec<i32>>())));
+    println!("{:?}", Solution::inorder_traversal(build_btree(&(1..2i32.pow(2)).collect::<Vec<i32>>())));
+    println!("{:?}", Solution::inorder_traversal(build_btree(&(1..2i32.pow(1)).collect::<Vec<i32>>())));
+    println!("{:?}", Solution::inorder_traversal(None));
     
     println!("Time elapsed:{} us", SystemTime::now().duration_since(start).unwrap().as_micros());
 }
